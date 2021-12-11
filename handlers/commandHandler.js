55b9b4494add35004b608afee2e3f0a2f00ec1ee
Yaknesik <81623470+Yaknesik@users.noreply.github.com>
2021-12-11 12:12:25 +0100

    Add files via upload

--- handlers/commandHandler.js
+++ handlers/commandHandler.js
@@ -1,1 +1,16 @@
+const {readdirSync} = require('fs');
 
+module.exports = (client) => {
+    readdirSync('./commands/').forEach(dir => {
+        const commands = readdirSync(`./commands/${dir}/`).filter(file => file.endsWith('.js'));
+        for(let file of commands){
+            let pull = require(`../commands/${dir}/${file}`);
+            if(pull.name){
+                client.commands.set(pull.name, pull);
+                console.log(`Test komendy: ${pull.name}`)
+            } else {
+                continue;
+            }if(pull.aliases && Array.isArray(pull.aliases)) pull.aliases.forEach(alias => client.aliases.set(alias, pull.name))
+        }
+    });
+}
