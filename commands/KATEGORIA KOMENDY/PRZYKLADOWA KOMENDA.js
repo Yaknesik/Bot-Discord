0c4655253241387aad6841de361072292eb928bc
Yaknesik <81623470+Yaknesik@users.noreply.github.com>
2022-03-18 20:08:40 +0100

    Update PRZYKLADOWA KOMENDA.js

--- commands/KATEGORIA KOMENDY/PRZYKLADOWA KOMENDA.js
+++ commands/KATEGORIA KOMENDY/PRZYKLADOWA KOMENDA.js
@@ -1,1 +1,10 @@
 /// Aby tworzyć komendy, musisz stworzyć pierw tzw. kategorię (jest to folder z nazwą np. 4FUN) oraz w tej kategorii komende w .js
+
+module.exports = {
+    name: `nazwakomendy`,
+    run: async(client, message, args) => {
+      
+      // i tu kod komendy :D
+
+   }
+}
